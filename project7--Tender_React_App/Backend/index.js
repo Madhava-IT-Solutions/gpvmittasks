c092db80561034d210472083e214147a0ddb9c3f
prasadvutukurimadhavaitsolutions <gangadharprasadvutukuri@madhavaitsolutions.in>
2025-01-29 12:12:51 +0530

    29-01-2025

--- project7--Tender_React_App/Backend/index.js
+++ project7--Tender_React_App/Backend/index.js
@@ -1,22 +1,23 @@
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 const homeRoutes = require("./routes/home");
 const applyRoutes = require("./routes/apply");
 const loginRoutes = require("./routes/login");
 const registrationsRoutes = require("./routes/registrations");
-const app = express();
-const port = 3001;
 
-const multer = require('multer');
+const app = express();
+const PORT = process.env.PORT || 3001; // Use Render's assigned port or default to 3001
+
+const multer = require("multer");
 const upload = multer();
 app.use(upload.any());
 
 // Middleware
-app.use(cors());  // Allow cross-origin requests
-app.use(express.json());  // Parse JSON request bodies
-app.use(express.urlencoded({ extended: true })); 
+app.use(cors()); // Allow cross-origin requests
+app.use(express.json()); // Parse JSON request bodies
+app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded bodies
 
@@ -32,9 +33,16 @@ app.use((err, req, res, next) => {
   res.status(500).send("Server error occurred");
 });
 
-
-
 // Start server
-app.listen(port, () => {
-  console.log(`Backend server running at http://localhost:${port}`);
+// app.listen(PORT, "0.0.0.0", () => {
+//   console.log(`Backend server running at `);
+// });
+
+
+app.listen(PORT, () => {
+  const serverURL = process.env.NODE_ENV === 'production' 
+    ? 'https://tenders-server.onrender.com' 
+    : `http://localhost:${PORT}`;   
+    
+    console.log(`Server is running on ${serverURL}`);
 });
