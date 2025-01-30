4bc290367493f24004f00cce19f431b2b072ac1e
prasadvutukurimadhavaitsolutions <gangadharprasadvutukuri@madhavaitsolutions.in>
2025-01-30 14:49:10 +0530

    30-01-2025

--- project7--Tender_React_App/Frontend/tenders/src/App.jsx
+++ project7--Tender_React_App/Frontend/tenders/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import ApplyPage from "./components/ApplyPage";
 import ArchivePage from "./components/ArchivePage";
